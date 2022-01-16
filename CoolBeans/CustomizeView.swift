d08f9967acdf2bb9edeb256c461896133dc80dfc
Alexey-frontend <lexer.work@gmail.com>
2022-01-16 22:00:05 +0300

    update customize body

--- CoolBeans/CustomizeView.swift
+++ CoolBeans/CustomizeView.swift
@@ -27,59 +27,9 @@ struct CustomizeView: View {
         Form {
             Section("Basic options") {
                 Picker("Size", selection: $size) {
-                    ForEach(sizeOptions.indices) { index in
-                        Text(sizeOptions[index])
-                    }
-                }
-                .pickerStyle(.segmented)
-
-                if drink.coffeeBased {
-                    Stepper("Extra shots: \(extraShots)", value: $extraShots, in: 0...8)
-                }
-
-                Toggle("Decaffeinated", isOn: $isDecaf)
-            }
-
-            Section("Customizations") {
-                Picker("Milk", selection: $milk) {
-                    ForEach(menu.milkOptions) { option in
-                        Text(option.name)
-                            .tag(option)
-                    }
-                }
-
-                if drink.coffeeBased {
-                    Picker("Syrup", selection: $syrup) {
-                        ForEach(menu.syrupOptions) { option in
-                            Text(option.name)
-                                .tag(option)
-                        }
-                    }
+                    
                 }
             }
-
-            Section("Estimates") {
-                Text("**Caffeine:** \(caffeine)mg")
-                Text("**Calories:** \(calories)")
-            }
-        }
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationTitle(drink.name)
-        .toolbar {
-            Button("Save") {
-                history.add(drink, size: sizeOptions[size], extraShots: extraShots, isDecaf: isDecaf, milk: milk, syrup: syrup, caffeine: caffeine, calories: calories)
-
-                dismiss()
-            }
-        }
-        .onAppear {
-            guard isFirstAppearance else { return }
-
-            if drink.servedWithMilk {
-                milk = menu.milkOptions[1]
-            }
-
-            isFirstAppearance = false
         }
     }
 }
